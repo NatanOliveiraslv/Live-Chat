804cf7b90cb8eabbf67e009e92b4e58a4d47fb17
NatanOliveiraslv <natanoliveiraslv14@gmail.com>
2026-07-13 21:44:16 -0300

    first commit

---
+++ src/main/java/com/br/livechatms/domain/ChatInput.java
@@ -0,0 +1,4 @@
+package com.br.livechatms.domain;
+
+public record ChatInput(String user, String message) {
+}
---
+++ src/main/java/com/br/livechatms/domain/ChatOutput.java
@@ -0,0 +1,4 @@
+package com.br.livechatms.domain;
+
+public record ChatOutput(String content) {
+}
